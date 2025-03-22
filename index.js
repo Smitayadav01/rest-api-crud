69da6e149d1ca880921f1a3f6e0e571d92205f02
Smitayadav01 <960smitayadav@gmail.com>
2025-03-22 21:34:32 +0530

    Added uuid

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const port=8080;
 const path=require("path");
+const{v4:uuidv4}=require("uuid");
 
 app.use(express.urlencoded({extended :true}));
 app.use(express.json());
@@ -12,17 +13,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts=[
     {
-        id:"1a",
+        id:uuidv4(),
         username:"apnacollege",
         content:"I love coding"
     },
     {
-        id:"2b",
+        id:uuidv4(),
         username:"smita",
         content:"Hard work is important to get the success"
     },
     {
-        id:"3c",
+        id:uuidv4(),
         username:"nikhilyadav",
         content:"I got selected for the mbms"
     }
@@ -38,15 +39,28 @@ app.get("/posts/new",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
     let {username,content}=req.body;
-    posts.push({username,content});
+    let id=uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts");
 })
 
 app.get("/posts/:id",(req,res)=>{
     let  {id} =req.params;
-    let post=posts.find((p)=> id === p.id);
-    res.render("show.ejs",{post});
+    let post=posts.find((p)=> p.id === id);
+    if (!post) {
+        return res.status(404).send("Post not found");
+    }
+    res.render("show.ejs",{post})
+})
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let Newcontent=req.body.content;
+    let post=posts.find((p)=>p.id===id);
+    post.content=Newcontent;
+    console.log(post);
+    res.send("patch  is working ");
 })
 app.listen(port,(req,res)=>{
-    console.log(`Server is running on post ${port}`);
+    console.log(`Server is running on port ${port}`);
 })
